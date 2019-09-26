501c4d549852576efea54e8773459369a8a6e27a
Dan <dsinger.sax@gmail.com>
2019-09-25 22:56:42 -0400

    Complete assignment

--- src/api-handler.js
+++ src/api-handler.js
@@ -1,24 +1,74 @@
-const users = {'dan': 1};
+const users = {};
 
 const serveUsers = (request, response, method) => {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    if (method === 'GET') {
-        let str = JSON.stringify(users);
-        response.write(str);
-    }
-    response.end();
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  if (method === 'GET') {
+    const str = JSON.stringify(users);
+    response.write(str);
+  }
+  response.end();
 };
 
-const serveNotFound = (request, response, method) => {
-    response.writeHead(404, {'Content-Type': 'application/json'});
+const serveNotReal = (request, response, method) => {
+  response.writeHead(404, { 'Content-Type': 'application/json' });
+  if (method === 'GET') {
     response.write(JSON.stringify({
-        message: "Resource not found"
+      message: 'Page is not real',
+      id: 'notFound',
     }));
-    response.end();
-}
+  }
+  response.end();
+};
+
+const serveNotFound = (request, response) => {
+  response.writeHead(404, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify({
+    message: 'Resource not found',
+    id: 'notFound',
+  }));
+  response.end();
+};
+
+const postUser = (request, response, method, bodyParams) => {
+  if (method !== 'POST') {
+    return;
+  }
+  let responseCode;
+  let message;
+  let id;
+
+  if (!bodyParams.name || !bodyParams.age) {
+    responseCode = 400;
+    id = 'badRequest';
+    message = 'Missing name and age parameters';
+  } else if (users[bodyParams.name]) {
+    responseCode = 204;
+    message = null;
+    id = null;
+    users[bodyParams.name].age = parseInt(bodyParams.age, 10);
+  } else {
+    responseCode = 201;
+    message = 'User created';
+    id = null;
+    const newUser = {
+      name: bodyParams.name,
+      age: parseInt(bodyParams.age, 10),
+    };
+    users[bodyParams.name] = newUser;
+  }
+  response.writeHead(responseCode, { 'Content-Type': 'application/json' });
+  if (message) {
+    const obj = { message };
+    if (id) obj.id = id;
+    response.write(JSON.stringify(obj));
+  }
+  response.end();
+};
 
 
 module.exports = {
-    serveUsers,
-    serveNotFound
-  };
+  serveUsers,
+  serveNotReal,
+  serveNotFound,
+  postUser,
+};
--- src/server.js
+++ src/server.js
@@ -1,30 +1,40 @@
-const http = require("http");
-const url = require("url");
-const query = require("querystring");
-const staticFileHandler = require("./static-file-handler.js");
-const apiHandler = require("./api-handler.js");
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const staticFileHandler = require('./static-file-handler.js');
+const apiHandler = require('./api-handler.js');
 
 const router = {
-    'GET': {
-        "/": (req, res) => staticFileHandler.serveStaticFile(req, res, "../client/client.html", "text/html"),
-        "/style.css": (req, res) => staticFileHandler.serveStaticFile(req, res, "../client/style.css", "text/css"),
-        "/getUsers": apiHandler.serveUsers
-    },
-    notFound: apiHandler.serveNotFound
-}
+  '/': (req, res) => staticFileHandler.serveStaticFile(req, res, '../client/client.html', 'text/html'),
+  '/style.css': (req, res) => staticFileHandler.serveStaticFile(req, res, '../client/style.css', 'text/css'),
+  '/getUsers': apiHandler.serveUsers,
+  '/notReal': apiHandler.serveNotReal,
+  '/addUser': apiHandler.postUser,
+  notFound: apiHandler.serveNotFound,
+};
+
+const parseBody = (request, callback) => {
+  const body = [];
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+    callback(bodyParams);
+  });
+};
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 http.createServer((request, response) => {
-    const parsedURL = url.parse(request.url);
-    const queryParams = query.parse(parsedURL.query);
-    let method = request.method;
-    if (!method) {
-        method = 'GET';
-    }
-    if (router[method][parsedURL.pathname]) {
-        router[method][parsedURL.pathname](request, response, method);
+  const parsedURL = url.parse(request.url);
+  const method = request.method;
+
+  parseBody(request, (bodyParams) => {
+    if (router[parsedURL.pathname]) {
+      router[parsedURL.pathname](request, response, method, bodyParams);
     } else {
-        router.notFound(request, response);
+      router.notFound(request, response, method, bodyParams);
     }
-    
+  });
 }).listen(port);
